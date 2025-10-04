155da2be42253d433c73dfa441ba6dd1e4fc6343
Preet Arora <141331002+Casper3004@users.noreply.github.com>
2025-10-04 10:40:24 +0530

    fly deploy

--- packages/ui/src/websocketContext.tsx
+++ packages/ui/src/websocketContext.tsx
@@ -23,7 +23,7 @@ export const WebSocketProvider = ({ children }: { children: React.ReactNode }) =
     const token = session?.user?.token;
     if (!token) return;
 
-    const ws = new WebSocket(`https://chatwebsite-4h13.onrender.com/?token=${token}`);
+    const ws = new WebSocket(`https://chat-app-restless-silence-2413.fly.dev/?token=${token}`);
 
     ws.onopen = () => {
       console.log("WebSocket connected");
