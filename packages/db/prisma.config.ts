55bf13803174fe220b09240fe4a7a27bafbca930
Preet Arora <141331002+Casper3004@users.noreply.github.com>
2026-05-14 21:02:28 +0530

    Specified rootDir

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "prisma/config";
 import "dotenv/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
+   schema: "prisma/schema.prisma",
+   datasource: {
+      url: process.env.DATABASE_URL,
+   },
 });
