1f8f78e4ac3b5165b2906ec4d8f8451fa4cbf0a2
Preet Arora <141331002+Casper3004@users.noreply.github.com>
2026-05-15 15:35:51 +0530

    fixed tsconfig

--- packages/db/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import 'dotenv/config';
-export declare const client: PrismaClient<import("@prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/client").DefaultArgs>;
